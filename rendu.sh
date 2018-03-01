e5c7392fb01423ee900873e125c485b4c31604cd
Mathieu BIGARE <mathieu.bigare@epitech.eu>
2018-03-01 21:08:31 +0100

    changed score calculation

---
+++ rendu.sh
@@ -0,0 +1,6 @@
+./hashcode example_files/e_high_bonus.in   > output/e_high_bonus.out
+./hashcode example_files/d_metropolis.in   > output/d_metropolis.out
+./hashcode example_files/c_no_hurry.in  > output/c_no_hurry.out
+./hashcode example_files/b_should_be_easy.in > output/b_should_be_easy.out
+./hashcode example_files/a_example.in > output/a_example.out
+zip hashcode.zip hashcode
